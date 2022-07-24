329648b1e3f704da2b727bcdd8723139cd4fa79b
xjhqre <859425698@qq.com>
2022-07-24 15:24:14 +0800

    dev commit，elasticsearch相关3

--- offline.py
+++ offline.py
@@ -12,7 +12,6 @@ errorPaths = []  # 存放提取错误的图片路径
 
 if __name__ == '__main__':
     fe = FeatureExtractor()
-    # trainPath = glob.glob('./static/img/*')  # 被检索的图片路径
     trainPath = glob.glob('F:/ACG/出处归档/*')  # 被检索的图片路径
     cnt = 0
 
@@ -32,7 +31,6 @@ if __name__ == '__main__':
             errorPaths.append(image)
         else:
             name = image.rsplit("\\")[1]
-            # imgUrl = "./static/img/" + image.rsplit("\\")[1]  # OSS
             imgUrl = "https://chuchu-xjhqre.oss-cn-hangzhou.aliyuncs.com/img/" + image.rsplit("\\")[1]  # OSS
 
             doc = {'url': imgUrl, 'feature': feature,
--- server_oss.py
+++ server_oss.py
@@ -13,8 +13,7 @@ fe = FeatureExtractor()
 
 es = Elasticsearch([{'host': '1.15.88.204', 'port': 9200}], timeout=3600)
 
-imgPrefix = "./static/img/"
-# imgPrefix = "https://xxx.oss-cn-hangzhou.aliyuncs.com/img/"
+imgPrefix = "https://xxx.oss-cn-hangzhou.aliyuncs.com/img/"
 
 def feature_search(query):
     global es
